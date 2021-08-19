e4c0f4448957d984089fbd062022c2366e41d31c
ngothanhhoan <84465650+ngothanhhoan@users.noreply.github.com>
2021-08-19 23:38:56 +0700

    Add files via upload

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ var fib = [];
 fib[0] = 0;
 fib[1] = 1;
 for (i = 2; i <= 1000; i++) {
-  fib[i] = fib[i - 2] + fib[i - 1]; //số sau bằng tổng của 2 số trước liền kề
+  fib[i] = fib[i - 2] + fib[i - 1]; 
 }
 
 console.log(fib)
